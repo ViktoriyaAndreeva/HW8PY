7a306a07e6d99afe0aac455bb01cc29be5a13fc1
Viktoriya <anviktoriya@yandex.ru>
2022-10-01 17:41:42 +0300

    Add do mark

--- function.py
+++ function.py
@@ -2,8 +2,6 @@ import utils as u
 import re
 import db
 
-# contacts = []
-
 def multi_input(titles):
     result = []
     for title in titles:
@@ -22,6 +20,7 @@ def view_contact_teacher():
     contacts = db.read("data_teacher.txt")
     print(contacts)
 
+
 def add_contact_student():
     print('Добавляем контакт ученика:')
     student = multi_input([
@@ -33,6 +32,7 @@ def add_contact_student():
 
     student_id = db.append("data_student.txt", student)
     print(f"Ученик успешно добавлен с id: '{student_id}'")
+
 
 def add_contact_teacher():
     print('Добавляем контакт учителя:')
@@ -48,25 +48,24 @@ def add_contact_teacher():
     print(f"Учитель успешно добавлен с id: '{teacher_id}'")
 
 
+# def delete_contact_student():  # удалить запись с бд взяв с консоли
+#     print('Удаляем контакт ученика')
+#     student = multi_input([
+#         "Фамилию",
+#         "Имя"
+#     ])
+#     student = db.remove("data_student.txt", student)
+#     print(f"Ученик успешно удален : '{student}'")
 
-def delete_contact_student():  # удалить запись с бд взяв с консоли
-    print('Удаляем контакт ученика')
-    student = multi_input([
-        "Фамилию",
-        "Имя"
-    ])
-    student_del = db.remove("data_student.txt", student)
-    print(f"Ученик успешно удален : '{student_del}'")
 
-
-def delete_contact_teacher():  # удалить запись с бд взяв с консоли
-    print('Удаляем контакт учителя')
-    teacher = multi_input([
-        "Фамилию",
-        "Имя"
-    ])
-    teacher_del = db.remove("data_student.txt", teacher)
-    print(f"Ученик успешно удален : '{teacher_del}'")
+# def delete_contact_teacher():  # удалить запись с бд взяв с консоли
+#     print('Удаляем контакт учителя')
+#     teacher = multi_input([
+#         "Фамилию",
+#         "Имя"
+#     ])
+#     teacher = db.remove("data_student.txt", teacher)
+#     print(f"Ученик успешно удален : '{teacher}'")
 
 def see_mark():
     id_student = input("Для просмотра оценок введите свой id:") 
@@ -74,24 +73,23 @@ def see_mark():
     search = False
     for lin in marks:
         if id_student in lin:
-            print(lin, end='\n')
+            print("Ваши оценки в дневнике:", lin, end='\n')
             search = True
     if not search:
-        print('Вы ввели не свой id')
+        print('Вы ввели не свой id!')
 
 
 def do_mark():
-    id_student = input("Чтобы поставить оценку введите id ученика:") 
-    marks = db.read("gradebook.txt")
-    search = False
-    for lin in marks:
-        if id_student in lin:
-            mark_id = db.append("gradebook.txt", marks)
-            print(f"Оценка добавлена для id: '{mark_id}'")
-            search = True
-    if not search:
-        print('Вы ввели неверный id ученика')
-
+    print('Выставляем оценку:')
+    marks = multi_input([
+        "id",
+        "Предмет",
+        "Дата",
+        "Оценка"
+    ])
+    marks = db.append("gradebook.txt", marks)
+    print(f"Оценка успешно добавлена, номер записи {marks}")
+    
 
 # def delete_contact_teacher():  # удалить запись с бд взяв с консоли
     
@@ -111,6 +109,18 @@ def do_mark():
 #             result = pattern.search(lin)
 #             if result is None:
 #                 f.write(lin)
+# def delete_contact_teacher(value = input("Введите id учителя, запись о котором подлежит удалению")):
+#     fullpath = db.os.path.join(db.DB_DIR, "data_teacher.txt")
+    
+#     data = db.read("data_teacher.txt")
+#     new_data = [";".join(record) for record in data if record[1] != value]
+#     new_data = "\n".join(new_data)
+#     print("Контакт удален")
+    
+#     with open(fullpath, "w", encoding="utf-8") as f:
+#         f.write(new_data + "\n")
+    
+
 
 def quit():
   print("До встречи!!!")
